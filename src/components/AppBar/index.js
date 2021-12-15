19519a243919390a14096cab2d6be0330e15e458
isa <5445946+icortes@users.noreply.github.com>
2021-12-14 19:52:35 -0800

    menu items created for desktop and mobile

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -47,6 +47,41 @@ export default function ResponsiveAppBar() {
               color='inherit'>
               <MenuIcon />
             </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+              keepMounted
+              transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}>
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign='center'>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant='h6'
+            noWrap
+            component='div'
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'flex', md: 'none' },
+            }}>
+            Isaac Cortes Hernandez
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}>
+                {page}
+              </Button>
+            ))}
           </Box>
         </Toolbar>
       </Container>
