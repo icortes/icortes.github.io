f1c0e58d98492c5b4be21d56d6065314bb01034b
isa <5445946+icortes@users.noreply.github.com>
2021-12-17 02:09:58 -0800

    fixed text overflow on mobile view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import { useTheme, Grid } from '@mui/material';
+import { useTheme, Grid, createTheme, Paper } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppBar from './components/AppBar';
 import Footer from './components/Footer';
 import AboutMe from './pages/AboutMe';
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
   return (
-    <Router>
-      <AppBar />
-      <Grid
-        container
-        spacing={0}
-        direction='column'
-        alignItems='center'
-        justifyContent='center'>
-        <Routes>
-          <Route exact path='/' element={<AboutMe />} />
-        </Routes>
-        <Footer />
-      </Grid>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <Paper elevation={5}>
+        <Router>
+          <AppBar />
+          <Grid
+            container
+            spacing={0}
+            direction='column'
+            alignItems='center'
+            justifyContent='center'>
+            <Routes>
+              <Route exact path='/' element={<AboutMe />} />
+            </Routes>
+            <Footer />
+          </Grid>
+        </Router>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
