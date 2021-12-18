f95f1807e1ebcde0f535f671d36565fb1453beff
isa <5445946+icortes@users.noreply.github.com>
2021-12-17 18:03:14 -0800

    styled porfolio h2 header

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,13 @@
-import { Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 
-export default function Portfolio() {
-  return <Typography>Portfolio</Typography>;
+export default function Portfolio(props) {
+  return (
+    <Box sx={{ width: '100vw', height: '100vh' }}>
+      <Container maxWidth='md'>
+        <Typography variant='h2' component={'div'} sx={{ mt: 5 }}>
+          Portfolio
+        </Typography>
+      </Container>
+    </Box>
+  );
 }
