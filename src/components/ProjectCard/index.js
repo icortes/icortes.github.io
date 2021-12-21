3dc8fd65b108fa8be234405bc21a1953f40d2806
isa <5445946+icortes@users.noreply.github.com>
2021-12-20 16:02:24 -0800

    created project card component

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,10 @@
+import { Card, CardActionArea } from "@mui/material";
+
+export default function ProjectCard({project}) {
+  
+  return (<Card sx={{ maxWidth: 345 }}>
+    <CardActionArea>
+      
+    </CardActionArea>
+  </Card>);
+}
