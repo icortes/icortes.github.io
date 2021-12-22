4f1e03a24b00d258e654e80422cf6b70a2acf935
isa <5445946+icortes@users.noreply.github.com>
2021-12-22 01:05:00 -0800

    created form for contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,32 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, TextField, Typography } from '@mui/material';
 
 export default function Contact(props) {
   return (
-    <Box sx={{ height: '100%', width: '100%' }}>
+    <Box sx={{ height: '100vh', width: '100%' }}>
       <Container maxWidth='md'>
-        
+        <Typography
+          variant='h2'
+          component={'div'}
+          sx={{ mt: 5 }}
+          style={{ textSizeAdjust: 'auto' }}>
+          Contact
+        </Typography>
+        <Box
+          component='form'
+          autoComplete='off'
+          flexDirection={'column'}
+          sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}>
+          <TextField required label='Name' variant='filled' />
+          <TextField required label='Email' variant='filled' />
+          <TextField
+            required
+            label='Message'
+            variant='filled'
+            multiline
+            maxRows={6}
+            rows={3}
+          />
+        </Box>
       </Container>
     </Box>
   );
