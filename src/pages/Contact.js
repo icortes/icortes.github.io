85d7ad01fbacde943f277e0f00762f3b6be0c1a8
isa <5445946+icortes@users.noreply.github.com>
2021-12-27 13:32:40 -0800

    changed form to email link and phone number

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,14 @@
-import { Box, Container, TextField, Typography } from '@mui/material';
+import { Email, PhoneIphone } from '@mui/icons-material';
+import {
+  Box,
+  Container,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 
 export default function Contact(props) {
   return (
@@ -12,20 +22,34 @@ export default function Contact(props) {
           Contact
         </Typography>
         <Box
-          component='form'
-          autoComplete='off'
-          flexDirection={'column'}
-          sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}>
-          <TextField required label='Name' variant='filled' />
-          <TextField required label='Email' variant='filled' />
-          <TextField
-            required
-            label='Message'
-            variant='filled'
-            multiline
-            maxRows={6}
-            rows={3}
-          />
+          sx={{
+            mt: 3,
+            width: '100%',
+            maxWidth: 360,
+            bgcolor: 'background.paper',
+          }}>
+          <nav aria-label='ways to contact me'>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton
+                  component='a'
+                  href='mailto:contact@isaaccortes.com'>
+                  <ListItemIcon>
+                    <Email />
+                  </ListItemIcon>
+                  <ListItemText primary='email me: contact@isaaccortes.com' />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component='a' href='tel:+1-650-296-2930'>
+                  <ListItemIcon>
+                    <PhoneIphone />
+                  </ListItemIcon>
+                  <ListItemText primary='phone #: (650) 296-2930' />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
         </Box>
       </Container>
     </Box>
