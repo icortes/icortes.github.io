b3b95fc14ca3796ac6e074dc3b561338601a1521
isa <5445946+icortes@users.noreply.github.com>
2021-12-27 15:21:28 -0800

    added mui list component to resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,35 @@
-import { Box, Container } from '@mui/material';
+import {
+  Box,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  Typography,
+} from '@mui/material';
 
 export default function Resume(props) {
   return (
     <Box sx={{ width: '100%', height: 'auto', overflow: 'auto' }}>
       <Container maxWidth='md'>
-        
+        <Typography component={'div'} variant='h2' sx={{ mt: 5 }}>
+          Resume
+        </Typography>
+        <Typography component={'div'} variant='h5' sx={{ mt: 3 }}>
+          Download my resume
+        </Typography>
+        <List
+          sx={{
+            mt: 3,
+            width: '100%',
+            maxWidth: 360,
+            bgcolor: 'background.paper',
+          }}
+          subheader={<ListSubheader>Front-end Proficiencies</ListSubheader>}>
+          <ListItem>
+            <ListItemText primary='HTML5' />
+          </ListItem>
+        </List>
       </Container>
     </Box>
   );
