00f2e2c6a3076ab37c8a8ccfef959912b33e7386
isa <5445946+icortes@users.noreply.github.com>
2021-12-27 19:12:52 -0800

    added list for front-end and back-end elements

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,6 +1,7 @@
 import {
   Box,
   Container,
+  Grid,
   List,
   ListItem,
   ListItemText,
@@ -18,18 +19,41 @@ export default function Resume(props) {
         <Typography component={'div'} variant='h5' sx={{ mt: 3 }}>
           Download my resume
         </Typography>
-        <List
-          sx={{
-            mt: 3,
-            width: '100%',
-            maxWidth: 360,
-            bgcolor: 'background.paper',
-          }}
-          subheader={<ListSubheader>Front-end Proficiencies</ListSubheader>}>
-          <ListItem>
-            <ListItemText primary='HTML5' />
-          </ListItem>
-        </List>
+        <Grid
+          container
+          rowSpacing={1}
+          columnSpacing={{ xs: 1, sm: 1, md: 1 }}
+          direction={'row'}
+          justifyContent={'space-evenly'}>
+          <Grid item sx={12} sm={6} md={6}>
+            <List
+              sx={{
+                mt: 3,
+                width: '100%',
+                bgcolor: 'background.paper',
+              }}
+              subheader={
+                <ListSubheader>Front-end Proficiencies</ListSubheader>
+              }>
+              <ListItem>
+                <ListItemText primary='HTML5' />
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item sx={12} sm={6} md={6}>
+            <List
+              sx={{
+                mt: 3,
+                width: '100%',
+                bgcolor: 'background.paper',
+              }}
+              subheader={<ListSubheader>Back-end Proficiencies</ListSubheader>}>
+              <ListItem>
+                <ListItemText primary='HTML5' />
+              </ListItem>
+            </List>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
