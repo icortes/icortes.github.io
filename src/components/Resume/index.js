c204c0e101f86254f1093ab59ee584169c40babb
isa <5445946+icortes@users.noreply.github.com>
2022-01-06 23:23:35 -0800

    created resume component and added it to home page

---
+++ src/components/Resume/index.js
@@ -0,0 +1,67 @@
+import {
+  Container,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  Typography,
+} from '@mui/material';
+
+export default function Resume({ proficiencies, resume }) {
+  return (
+    <Container className='Resume'>
+      <Typography component={'div'} variant='h2' sx={{ mt: 5 }}>
+        Resume
+      </Typography>
+      <Typography component={'div'} variant='h5' sx={{ mt: 3 }}>
+        Download my{' '}
+        <Link href={resume} target={'_blank'}>
+          resume
+        </Link>
+      </Typography>
+      <Grid
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 1, md: 1 }}
+        direction={'row'}
+        justifyContent={'space-evenly'}>
+        <Grid item xs={12} sm={6} md={6}>
+          <List
+            sx={{
+              mt: 3,
+              width: '100%',
+              bgcolor: 'background.paper',
+            }}
+            subheader={<ListSubheader>Front-end Proficiencies</ListSubheader>}>
+            {proficiencies[0]['front-end'].map((proficiency, index) => {
+              return (
+                <ListItem key={index}>
+                  <ListItemText primary={proficiency} />
+                </ListItem>
+              );
+            })}
+          </List>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <List
+            sx={{
+              mt: 3,
+              width: '100%',
+              bgcolor: 'background.paper',
+            }}
+            subheader={<ListSubheader>Back-end Proficiencies</ListSubheader>}>
+            {proficiencies[0]['back-end'].map((proficiency, index) => {
+              return (
+                <ListItem key={index}>
+                  <ListItemText primary={proficiency} />
+                </ListItem>
+              );
+            })}
+          </List>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,22 @@
-import { Box, Container } from "@mui/material";
-import AboutMe from "../components/AboutMe";
-import Contact from "../components/Contact";
-import Portfolio from "../components/Portfolio";
+import { Box, Container } from '@mui/material';
+import AboutMe from '../components/AboutMe';
+import Contact from '../components/Contact';
+import Portfolio from '../components/Portfolio';
 import ProjectCard from '../components/ProjectCard';
 import projects from '../db/projects.json';
+import proficiencies from '../db/proficiencies.json';
+import resume from '../assets/doc/IsaacCortesHernResume.pdf';
+import Resume from '../components/Resume';
 
 export default function HomePage(props) {
-  return <Box sx={{ width: '100%', height: 'auto' }}>
-    <Container maxWidth='md'>
-      <AboutMe />
-      <Portfolio projects={projects} ProjectCard={ProjectCard} />
-      <Contact />
-    </Container>
-  </Box>;
+  return (
+    <Box sx={{ width: '100%', height: 'auto' }}>
+      <Container maxWidth='md'>
+        <AboutMe />
+        <Portfolio projects={projects} ProjectCard={ProjectCard} />
+        <Contact />
+        <Resume proficiencies={proficiencies} resume={resume} />
+      </Container>
+    </Box>
+  );
 }
